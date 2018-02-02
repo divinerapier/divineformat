48e4c15aec642b52aa9ae6b87a1addfb3f43592d
divinerapier <poriter.coco@gmail.com>
2018-02-02 14:03:52 +0800

    updated benchmark

--- jsonformat_test.go
+++ jsonformat_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func J() {
+func J0(b *testing.B) {
 	formatter := NewJSONFormatter().
 		Str("string0", "value0").
 		Str("string1", "value1").
@@ -26,32 +26,134 @@ func J() {
 		Int("int4", 4).
 		Ints("ints0", 0, 1, 2, 3, 4)
 
-	formatter.Flush()
+	l := len(formatter.Flush())
 	formatter.Release()
 
-	// fmt.Println(formatter.OutputString())
+	b.SetBytes(int64(l))
+}
+
+func J1(b *testing.B) {
+	formatter := NewJSONFormatter()
+	formatter = formatter.Str("string0", "value0")
+	formatter = formatter.Str("string1", "value1")
+	formatter = formatter.Str("string2", "value2")
+	formatter = formatter.Str("string3", "value3")
+	formatter = formatter.Str("string4", "value4")
+	formatter = formatter.Strs("strings0", "v0", "v1", "v2", "v3", "v4")
+	formatter = formatter.Bool("bool0", 1 == 0)
+	formatter = formatter.Bool("bool1", 1 == 1)
+	formatter = formatter.Bool("bool2", 1 == 0)
+	formatter = formatter.Bool("bool3", 1 == 1)
+	formatter = formatter.Bool("bool4", 1 == 0)
+	formatter = formatter.Bools("bools0", 1 == 1, 1 == 0, 1 == 1, 1 == 2)
+	formatter = formatter.Int("int0", 0)
+	formatter = formatter.Int("int1", 1)
+	formatter = formatter.Int("int2", 2)
+	formatter = formatter.Int("int3", 3)
+	formatter = formatter.Int("int4", 4)
+	formatter = formatter.Ints("ints0", 0, 1, 2, 3, 4)
+
+	l := len(formatter.Flush())
+	formatter.Release()
+
+	b.SetBytes(int64(l))
 }
 
 const tpl = `{"%v":"%v","%v":"%v","%v":"%v","%v":"%v","%v":"%v","%v":["%v","%v","%v","%v","%v"],"%v":%v,"%v":%v,"%v":%v,"%v":%v,"%v":%v,"%v":[%v,%v,%v,%v],"%v":%v,"%v":%v,"%v":%v,"%v":%v,"%v":%v,"%v":[%v,%v,%v,%v,%v]}`
 
-func F() string {
-	return fmt.Sprintf(tpl, "string0", "value0", "string1", "value1", "string2", "value2", "string3", "value3", "string4", "value4", "strings0", "v0", "v1", "v2", "v3", "v4", "bool0", false, "bool1", true, "bool2", false, "bool3", true, "bool4", false, "bools0", true, false, true, false, "int0", 0, "int1", 1, "int2", 2, "int3", 3, "int4", 4, "ints0", 0, 1, 2, 3, 4)
+func F0(b *testing.B) string {
+	s := fmt.Sprintf(tpl, "string0", "value0", "string1", "value1", "string2", "value2", "string3", "value3", "string4", "value4", "strings0", "v0", "v1", "v2", "v3", "v4", "bool0", false, "bool1", true, "bool2", false, "bool3", true, "bool4", false, "bools0", true, false, true, false, "int0", 0, "int1", 1, "int2", 2, "int3", 3, "int4", 4, "ints0", 0, 1, 2, 3, 4)
+
+	b.SetBytes(int64(len(s)))
+	return s
 }
 
-func BenchmarkJSON(b *testing.B) {
+func F1(b *testing.B) string {
+	s := fmt.Sprintf(`{"%v":"`, "string0")
+	s += fmt.Sprintf(`%v"`, "value0")
+	s += fmt.Sprintf(`,"%v":"`, "string1")
+	s += fmt.Sprintf(`%v","`, "value1")
+	s += fmt.Sprintf(`%v`, "string2")
+	s += fmt.Sprintf(`":"`)
+	s += fmt.Sprintf(`%v","`, "value2")
+	s += fmt.Sprintf(`%v`, "string3")
+	s += fmt.Sprintf(`":"`)
+	s += fmt.Sprintf(`%v","`, "value3")
+	s += fmt.Sprintf(`%v":"`, "string4")
+	s += fmt.Sprintf(`%v","`, "value4")
+	s += fmt.Sprintf(`%v":["`, "strings0")
+	s += fmt.Sprintf(`%v","`, "v0")
+	s += fmt.Sprintf(`%v","`, "v1")
+	s += fmt.Sprintf(`%v","`, "v2")
+	s += fmt.Sprintf(`%v","`, "v3")
+	s += fmt.Sprintf(`%v"],"`, "v4")
+	s += fmt.Sprintf(`%v`, "bool0")
+	s += fmt.Sprintf(`":%v,`, false)
+	s += fmt.Sprintf(`"%v":`, "bool1")
+	s += fmt.Sprintf(`%v,"`, true)
+	s += fmt.Sprintf(`%v":`, "bool2")
+	s += fmt.Sprintf(`%v,"`, false)
+	s += fmt.Sprintf(`%v":`, "bool3")
+	s += fmt.Sprintf(`%v,"`, true)
+	s += fmt.Sprintf(`%v":`, "bool4")
+	s += fmt.Sprintf(`%v,"`, false)
+	s += fmt.Sprintf(`%v":[`, "bools0")
+	s += fmt.Sprintf(`%v,`, true)
+	s += fmt.Sprintf(`%v,`, false)
+	s += fmt.Sprintf(`%v,`, true)
+	s += fmt.Sprintf(`%v],"`, false)
+	s += fmt.Sprintf(`%v":`, "int0")
+	s += fmt.Sprintf(`%v,"`, 0)
+	s += fmt.Sprintf(`%v":`, "int1")
+	s += fmt.Sprintf(`%v,"`, 1)
+	s += fmt.Sprintf(`%v":`, "int2")
+	s += fmt.Sprintf(`%v,"`, 2)
+	s += fmt.Sprintf(`%v":`, "int3")
+	s += fmt.Sprintf(`%v,"`, 3)
+	s += fmt.Sprintf(`%v":`, "int4")
+	s += fmt.Sprintf(`%v,"`, 4)
+	s += fmt.Sprintf(`%v":[`, "ints0")
+	s += fmt.Sprintf(`%v,`, 0)
+	s += fmt.Sprintf(`%v,`, 1)
+	s += fmt.Sprintf(`%v,`, 2)
+	s += fmt.Sprintf(`%v,`, 3)
+	s += fmt.Sprintf(`%v]}`, 4)
+
+	b.SetBytes(int64(len(s)))
+	return s
+}
+
+func BenchmarkJSON_Chain(b *testing.B) {
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			J()
+			J0(b)
 		}
 	})
 }
 
-func BenchmarkSTD(b *testing.B) {
+func BenchmarkJSON_ManyTimes(b *testing.B) {
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			F()
+			J1(b)
+		}
+	})
+}
+
+func BenchmarkSTD_Once(b *testing.B) {
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			F0(b)
+		}
+	})
+}
+func BenchmarkSTD_ManyTimes(b *testing.B) {
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			F1(b)
 		}
 	})
 }
--- readme.md
+++ readme.md
@@ -7,6 +7,8 @@ $ go test -run=none -bench=. -benchmem -cpuprofile=cpu.out  -memprofile mem.out
 goos: darwin
 goarch: amd64
 pkg: github.com/divinerapier/divineformat
-BenchmarkJSON-4          5000000               406 ns/op             132 B/op          3 allocs/op
-BenchmarkSTD-4           1000000              1074 ns/op             320 B/op          1 allocs/op
+BenchmarkJSON_Chain-4            5000000               362 ns/op         814.81 MB/s         132 B/op          3 allocs/op
+BenchmarkJSON_ManyTimes-4        5000000               413 ns/op         713.40 MB/s         132 B/op          3 allocs/op
+BenchmarkSTD_Once-4              1000000              1066 ns/op         281.42 MB/s         320 B/op          1 allocs/op
+BenchmarkSTD_ManyTimes-4          200000              8231 ns/op          36.45 MB/s        9508 B/op         97 allocs/op
 ```
